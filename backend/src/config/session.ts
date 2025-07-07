b994fbfa50598e13554ec9b782e9d9fa12853c54
Eren ÇELİK <126926648+Erenen1@users.noreply.github.com>
2025-07-07 22:00:39 +0300

    sameSite setted as none

--- backend/src/config/session.ts
+++ backend/src/config/session.ts
@@ -9,7 +9,7 @@ export const sessionConfig = session({
     secure: false, 
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, 
-    sameSite: 'strict',
+    sameSite: 'none',
     path: '/',
     partitioned: false,
   },
