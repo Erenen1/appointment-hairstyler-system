524652f3e9fdbed9f507196c180357b6d546b61c
Eren ÇELİK <126926648+Erenen1@users.noreply.github.com>
2025-08-13 23:56:00 +0300

    chore: update index.ts

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -2,6 +2,5 @@ export { ApiSuccess } from './ApiResponse';
 export { ApiError } from './ApiError';
 export type { ErrorType } from './ApiError';
 export { HashUtils } from './hashUtils';
-export { handleControllerError } from './controllerUtils';
 export { JwtUtils } from './jwtUtils';
 export type { JwtPayload } from './jwtUtils'; 
